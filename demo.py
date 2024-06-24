7b5c19d5859870727ac5d955f457b61e54d20e2d
cloudinstitution <cloudinstitution@gmail.com>
2024-06-24 12:57:39 +0530

    Reapply " 7 th commit"
    This reverts commit b5a92e4646f39bfec614e442e1fe6f1d6408f20c.

--- demo.py
+++ demo.py
@@ -4,11 +4,8 @@ print(a)
 print(b)
 print(a+b)
 <<<<<<< HEAD
-<<<<<<< HEAD
 print("hello")
 print('new user')
 
-=======
->>>>>>> parent of 4dd3c55 ( 7 th commit)
 =======
 >>>>>>> parent of 4dd3c55 ( 7 th commit)
